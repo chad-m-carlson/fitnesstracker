50b520706f32972b8b4a503786b0df948e74946e
Chad <chad.m.carlson@gmail.com>
2019-08-16 22:12:05 -0600

    heroku deploy

--- db/migrate/20190731003505_create_work_outs.rb
+++ db/migrate/20190731003505_create_work_outs.rb
@@ -4,7 +4,7 @@ class CreateWorkOuts < ActiveRecord::Migration[5.2]
       t.string :date
       t.integer :workout_id
       t.belongs_to :exercise, foreign_key: true
-      # t.belongs_to :rep, foreign_key: true
+      t.belongs_to :rep, foreign_key: true
 
       t.timestamps
     end
