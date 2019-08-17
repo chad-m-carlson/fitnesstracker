b2e856d32bf223f219d76c5d258497bf7fc456b6
Chad <chad.m.carlson@gmail.com>
2019-08-16 22:23:10 -0600

    heroku deploy

--- db/migrate/20190816202529_remove_rep_id_column_from_work_outs.rb
+++ db/migrate/20190816202529_remove_rep_id_column_from_work_outs.rb
@@ -1,5 +1,5 @@
 class RemoveRepIdColumnFromWorkOuts < ActiveRecord::Migration[5.2]
   def change
-    remove_column :work_outs, :rep_id, :bigint
+    # remove_column :work_outs, :rep_id, :bigint
   end
 end
