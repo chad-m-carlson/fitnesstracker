44b04ad332e8838e3f2a010b0e1f97c9562f9cca
Chad <chad.m.carlson@gmail.com>
2019-08-17 15:20:35 -0600

    find workout via date picker

--- app/models/work_out.rb
+++ app/models/work_out.rb
@@ -1,13 +1,13 @@
 class WorkOut < ApplicationRecord
   has_and_belongs_to_many :exercises
 
-  def self.find_work_out
+  def self.find_work_out(date)
     WorkOut.find_by_sql(["
       SELECT e.name, wo.rep_pace, wo.rep_amount, e.id
       FROM work_outs AS wo
       LEFT JOIN exercises AS e
       ON e.id = wo.exercise_id
-      WHERE wo.date = '8/17/2019'
+      WHERE wo.date = '#{date}'
     "])
   end
 
