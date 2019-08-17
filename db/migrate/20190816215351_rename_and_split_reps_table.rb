44513623f37accd5e173fc09ca99ddfb7ba0f282
Chad <chad.m.carlson@gmail.com>
2019-08-16 22:29:14 -0600

    heroku deploy

--- db/migrate/20190816215351_rename_and_split_reps_table.rb
+++ db/migrate/20190816215351_rename_and_split_reps_table.rb
@@ -1,9 +1,15 @@
 class RenameAndSplitRepsTable < ActiveRecord::Migration[5.2]
   def change
-    remove_column :reps, :pace, :string
-    rename_table :reps, :rep_amounts
+    # remove_column :reps, :pace, :string
+    # rename_table :reps, :rep_amounts
     create_table :rep_paces do |t|
       t.string :pace
+      
+      t.timestamps
+    end
+    
+    create_table :rep_amounts do |t|
+      t.string :amount
       
       t.timestamps
     end
